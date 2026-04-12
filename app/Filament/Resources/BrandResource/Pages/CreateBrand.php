f53ba7d7aeb09cfe397b9b9875a4dcf1a7988d6a
hakuuu <ahaqqurrahman12@gmail.com>
2026-04-12 21:57:19 +0700

    auto redirect setelah create dan update

--- app/Filament/Resources/BrandResource/Pages/CreateBrand.php
+++ app/Filament/Resources/BrandResource/Pages/CreateBrand.php
@@ -8,5 +8,11 @@ use Filament\Resources\Pages\CreateRecord;
 
 class CreateBrand extends CreateRecord
 {
+
     protected static string $resource = BrandResource::class;
+
+    protected function getRedirectUrl(): string
+{
+    return $this->getResource()::getUrl('index');
+}
 }
